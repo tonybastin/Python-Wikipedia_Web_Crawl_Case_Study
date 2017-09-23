8afe6084a0eadac9fe24c6ed7e090ec6c997648e
Tony Bastin <tonyship30@gmail.com>
2017-09-23 13:45:07 -0400

    Added continue_crawl function

---
+++ wiki_web_crawler.py
@@ -0,0 +1,15 @@
+
+
+
+def continue_crawl(search_history, target_url, max_steps=25):
+    if search_history[-1] == target_url:
+        print("We've found the target article!")
+        return False
+    elif len(search_history) > max_steps:
+        print("The search has gone on suspiciously long, aborting search!")
+        return False
+    elif search_history[-1] in search_history[:-1]:
+        print("We've arrived at an article we've already seen, aborting search!")
+        return False
+    else:
+        return True
